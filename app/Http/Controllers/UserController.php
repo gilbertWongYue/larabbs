48e93fb034ef26ad4bdb21a1fa56c2a8fc7b982d
vagrant <vagrant@homestead>
2018-06-21 11:13:19 +0000

    basic user profile editing

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest; //自定义表单验证类
 use App\Http\Controllers\Controller;
 use App\Models\User;
 class UserController extends Controller
@@ -13,8 +13,15 @@ class UserController extends Controller
         return view('users.show', compact('user'));
     }
 
-    public function edit()
+    public function edit(User $user)
     {
+        return view('users.edit', compact('user'));
+    }
 
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料已更新成功');
+        //如果成功，with()方法会向Session 中写入success，可以从Session中获取success 的值
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //table 栏位默认长度为191
+        Schema::defaultStringLength(191);
+        //时间格式改为中文格式
+        \Carbon\Carbon::setLocale('zh');
+
     }
 
     /**
