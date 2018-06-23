d44e10f63a730b5b51dfeae1673ba12cdf7beee2
vagrant <vagrant@homestead>
2018-06-23 06:52:46 +0000

    user can upload avatar

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\UserRequest; //自定义表单验证类
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Handlers\ImageUploadHandler;
 class UserController extends Controller
 {
     //
@@ -18,9 +19,18 @@ class UserController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, User $user, ImageUploadHandler $uploader)
     {
-        $user->update($request->all());
+        $data = $request->all();
+
+        if($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatar', $user->id, 362);
+            if($result) {
+                //将图片保存的路径返回
+                $data['avatar'] = $result['path'];
+            }
+        }
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料已更新成功');
         //如果成功，with()方法会向Session 中写入success，可以从Session中获取success 的值
     }
