c85336c9ccc0982890dfe733ffe1211ccd595d88
vagrant <vagrant@homestead>
2018-06-26 11:01:51 +0000

    seeding topics and users data

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //获取faker实例
+        $faker = app(Faker\Generator::class);
+        // 头像假数据
+        $avatars = [
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/Lhd1SHqu86.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/LOnMrqbHJn.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/xAuDMxteQy.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png?imageView2/1/w/200/h/200',
+        ];
+
+        $users = factory(User::class)
+                        ->times(10)
+                        ->make()
+                        ->each(function ($user, $index)
+                            use ($faker, $avatars)
+                            {
+                                //从头像数据中随机取出一个并赋值
+                                $user->avatar = $faker->randomElement($avatars);
+                            });
+        //让隐藏的字段可见，并将数据集合转化为数组
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+        //插入数据库
+        User::insert($user_array);
+
+        //单独设置第一个用户
+        $user = User::find(1);
+        $user->name = 'gilbert';
+        $user->email = 'gilbert@123.com';
+        $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
+        $user->save();
+
+    }
+}
