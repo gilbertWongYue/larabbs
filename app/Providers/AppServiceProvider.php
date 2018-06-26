41e2f82b5238a78212307e88f14c92ae8be75b0c
vagrant <vagrant@homestead>
2018-06-26 09:41:21 +0000

    generated topics

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
+	{
+		\App\Models\User::observe(\App\Observers\UserObserver::class);
+		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
+
         //table 栏位默认长度为191
         Schema::defaultStringLength(191);
         //时间格式改为中文格式
