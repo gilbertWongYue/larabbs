64ff0117f8399eab831d6d5ebc9dcb3069b17798
vagrant <vagrant@homestead>
2018-07-03 08:04:38 +0000

    topic order

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -16,4 +16,33 @@ class Topic extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    //本地作用域
+    public function scopeWithOrder($query, $order)
+    {
+        switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
+
+            default:
+                $query->recentReplied();
+                break;
+        }
+        // 预加载防止 N+1 问题
+        return $query->with('user', 'category');
+    }
+
+
+    public function scopeRecentReplied($query)
+    {
+        //根据最新回复时间排序
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    public function scopeRecent($query)
+    {
+        //根据创建时间排序
+        return $query->orderBy('created_at', 'desc');
+    }
 }
